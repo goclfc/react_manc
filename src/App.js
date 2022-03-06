88558a0018a4817d4cc95cf63f0165423cf2981b
GatsbyJr <gocha.berulava@gmail.com>
2022-03-07 00:03:29 +0400

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import { Switch, Route, BrowserRouter} from 'react-router-dom'
+import Header from './Components/header_footer/Header';
+import Footer from './Components/header_footer/Footer';
+import Home from './Components/Home/Home';
 function App() {
   return (
-  <>
-   hello
-  </>
-  );
+    <BrowserRouter>
+      <Header />
+      <Switch >
+        <Route path='/' exact component ={Home}/>
+
+       
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
 export default App;
