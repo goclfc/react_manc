1c2683674d242239f0065f5854686c7bb8dff86e
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-03-07 13:23:18 +0400

    formik
    validate inputs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route, BrowserRouter} from 'react-router-dom'
 import Header from './Components/header_footer/Header';
 import Footer from './Components/header_footer/Footer';
 import Home from './Components/Home/Home';
+import Signin from './Components/Signin';
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Switch >
+        <Route path='/sign_in' exact component ={Signin}/>
         <Route path='/' exact component ={Home}/>
 
        
